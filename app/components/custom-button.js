13c29b515aae7745a6f1b9021b5c398dba078215
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2022-07-27 09:52:39 +0530

    added custom-button component

---
+++ app/components/custom-button.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    click(e) {
+        if (e.target.children.length > 0) e.target.children[0].click();
+    },
+    mouseEnter(e) {
+        if (e.target.classList.contains("default-light-button")) {
+            e.target.classList.remove("default-light-button");
+            e.target.classList.add("hovered-light-button");
+        }
+    },
+    mouseLeave(e) {
+        if (e.target.classList.contains("hovered-light-button")) {
+            e.target.classList.remove("hovered-light-button");
+            e.target.classList.add("default-light-button");
+        }
+    }
+});
