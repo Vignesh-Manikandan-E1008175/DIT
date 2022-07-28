498c213f368d52589caea11a0f79e93e533548b3
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2022-07-28 15:04:58 +0530

    added Todo model

---
+++ app/models/todo.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+const { attr, belongsTo } = DS;
+
+export default DS.Model.extend({
+    userId: attr('number'),
+    title: attr(),
+    completed: attr('boolean', { defaultValue: false }),
+    createdAt: attr('date', {
+        defaultValue: function() {
+            return new Date();
+        }
+    })
+});
