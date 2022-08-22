1556397d98841cf1b3a86ad61e591c5dd0df47c3
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2022-08-22 18:25:07 +0530

    updated relationships and ID for model indexing

--- app/models/todo.js
+++ app/models/todo.js
@@ -4,6 +4,7 @@ const { attr, belongsTo } = DS;
 
 export default DS.Model.extend({
     userId: belongsTo('user'),
+    uid: attr(),
     title: attr(),
     completed: attr('boolean', { defaultValue: false }),
     createdAt: attr('date', {
--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
-    userId: attr('number'),
     userName: attr('string'),
     todos: hasMany('todo')
 });
