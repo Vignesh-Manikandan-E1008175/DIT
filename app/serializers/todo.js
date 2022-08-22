a1390ac41b24add62694f031c215722024906f4d
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2022-08-22 18:24:25 +0530

    updated serializers

--- app/serializers/todo.js
+++ app/serializers/todo.js
@@ -1,10 +1,20 @@
-import DS from 'ember-data';
+// import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
-    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-        payload = {
-            todos: payload
-        };
-        return this._super(store, primaryModelClass, payload, id, requestType);
-    }
+// export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
+//     attrs: {
+//         todos: { embedded: 'always' }
+//     },
+//     normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+//         payload = {
+//             todos: payload
+//         };
+//         return this._super(store, primaryModelClass, payload, id, requestType);
+//     }
+// });
+import FirebaseSerializer from "emberfire/serializers/firebase";
+
+export default FirebaseSerializer.extend({
+    attrs: {
+        todos: { embedded: "always" },
+    },
 });
